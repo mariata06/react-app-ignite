cca29792e82e9b99fb966a588e6b6a266cb29619
mariata06 <slinkywizard@mail.ru>
2023-02-06 18:14:55 +0300

    Добавляет рейтинг в описание игры

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -14,6 +14,8 @@ import xbox from '../img/xbox.svg';
 import nintendo from '../img/nintendo.svg';
 import apple from '../img/apple.svg';
 import gamepad from '../img/gamepad.svg';
+import starEmpty from '../img/star-empty.png';
+import starFull from '../img/star-full.png';
 
 const GameDetail = ( {pathId }) => {
     const navigate = useNavigate();
@@ -25,6 +27,21 @@ const GameDetail = ( {pathId }) => {
             navigate('/');
         }
     }
+
+    //Set game rating 
+    const getRating = () => {
+        const stars = [];
+        const rating = Math.floor(game.rating);
+        for(let i = 1; i <=5; i++) {
+            if (i <= rating) {
+                stars.push(<img alt="star" key={i} src={starFull}></img>)
+            } else {
+                stars.push(<img alt="star" key={i} src={starEmpty}></img>)
+            }
+        }
+        return stars;
+    };
+
 
     //GET platform images
     const getPlatform = (platform) => {
@@ -55,6 +72,7 @@ const GameDetail = ( {pathId }) => {
                             <div className='rating'>
                                 <motion.h3 layoutId={`title ${pathId}`}>{game.name}</motion.h3>
                                 <p>Rating: {game.rating}</p>
+                                {getRating()}
                             </div>
                             <Info>
                                 <h3>Platforms</h3>
@@ -127,7 +145,11 @@ const Stats = styled(motion.div)`
     display: flex;
     align-items: center;
     justify-content: space-between;
-
+    img {
+        width: 2rem;
+        height: 2rem;
+        display: inline;
+    }
 `;
 
 const Info = styled(motion.div)`
